06b7b8791142b74ad859af71ead2663e69aa3861
semtassta <semtassta@gmail.com>
2025-04-10 00:47:05 +0300

    performed isort

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 from core_utils.article.article import Article
 from core_utils.article.io import to_raw
 from core_utils.config_dto import ConfigDTO
-from core_utils.constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
+from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
 
 def is_valid_url(url: str) -> bool:
